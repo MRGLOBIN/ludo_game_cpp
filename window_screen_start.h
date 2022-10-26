48b4434f2e5bdfb01dc2533de1ca93f4d4ee28d2
Muhammad Asim <asimqwe2@gmail.com>
2022-10-26 10:00:00 +0500

    Update window_screen_start.h

--- window_screen_start.h
+++ window_screen_start.h
@@ -30,4 +30,34 @@ void delevlopers_info()
       outtextxy(300,70,"NAMES");
       delay(600);
       settextstyle(9,0,4);
+      setcolor(YELLOW);
+      outtextxy(205,130," > Syed Abdul Basit Kazmi");
+      delay(500);
+      setcolor(LIGHTCYAN);
+      outtextxy(205,160," > Muhammad Asim"); delay(500);
+      outtextxy(205,190," > Muhammad Ahmed"); delay(500);
+      outtextxy(205,220," > Syed Shayan Ali Shah");
+      setcolor(WHITE);
+      settextstyle(EUROPEAN_FONT,0,2);
+      outtextxy(205,365,"Press any key to continue...");
+
+      getch();
+
+      ///first member information////////////////
+
+      setcolor(LIGHTCYAN);
+      settextstyle(EUROPEAN_FONT,0,4);
+      for(int i=180;i>=20;i-=5)
+      {
+      cleardevice();
+      outtextxy(200,i,"Syed Abdul Basit Kazmi");
+      rectangle(160,i,680,i+35);
+      delay(20);
+      }
+      settextstyle(EUROPEAN_FONT,0,2);
+      delay(500);
+      setcolor(LIGHTBLUE);
+      readimagefile("sir.jpeg", 80, 100, 250, 290);
+      setcolor(YELLOW);
+      settextstyle(9,0,3);
 #endif // WINDOW_SCREEN_START_H_INCLUDED
