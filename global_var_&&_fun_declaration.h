7d43814946d30ce78770b1ae2914b7faacc364ac
Muhammad Asim <asimqwe2@gmail.com>
2022-11-03 10:00:00 +0500

    Update global_var_&&_fun_declaration.h

---
+++ global_var_&&_fun_declaration.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_VAR_&&_FUN_DECLARATION_H_INCLUDED
+#define GLOBAL_VAR_&&_FUN_DECLARATION_H_INCLUDED
+
+//for variables
+int no_of_turns = 0, dice_return = 0, another_chance = 0,
+    redpaw, redflag = 0, greenpaw, greenflag = 0, yellowpaw, yellowflag = 0, bluepaw, blueflag = 0,
+    screenx = GetSystemMetrics(SM_CXSCREEN) / 2,
+    screeny = GetSystemMetrics(SM_CYSCREEN) / 2,
+    random1, random2,
+    player_mod, ret_choice_2, ret_choice_3,
+    check_var,
+    var,
+#endif // GLOBAL_VAR_&&_FUN_DECLARATION_H_INCLUDED
