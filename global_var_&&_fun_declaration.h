ce8dc04606c26388257289a162bad9107cbd7b1c
Muhammad Asim <asimqwe2@gmail.com>
2022-11-04 10:00:00 +0500

    Update global_var_&&_fun_declaration.h

--- global_var_&&_fun_declaration.h
+++ global_var_&&_fun_declaration.h
@@ -10,4 +10,14 @@ int no_of_turns = 0, dice_return = 0, another_chance = 0,
     player_mod, ret_choice_2, ret_choice_3,
     check_var,
     var,
+    page = 0,
+    main_window,
+    choice3_1= 0,
+    choice3_2= 0,
+    choice3_3= 0,
+    choice3_4= 0;
+bool win = true;
+
+//for functions
+
 #endif // GLOBAL_VAR_&&_FUN_DECLARATION_H_INCLUDED
