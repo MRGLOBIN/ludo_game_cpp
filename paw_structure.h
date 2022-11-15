ae9f70ec390dc0856301f74e69fc251fd889c510
Muhammad Asim <asimqwe2@gmail.com>
2022-11-15 10:00:00 +0500

    Update paw_structure.h

--- paw_structure.h
+++ paw_structure.h
@@ -648,4 +648,112 @@ struct yellowcolorpaw
       {
           x = 206, y = 405;
       }
+      else if (dice_number == 27)
+      {
+          x = 153, y = 405;
+      }
+      else if (dice_number == 28)
+      {
+          x = 100, y = 405;
+      }
+      else if (dice_number == 29)
+      {
+          x = 47, y = 405;
+      }
+      else if (dice_number == 30)
+      {
+          x = 47, y = 360;
+      }
+      else if (dice_number == 31)
+      {
+          x = 47, y = 315;
+      }
+      else if (dice_number == 32)
+      {
+          x = 100, y = 315;
+      }
+      else if (dice_number == 33)
+      {
+          x = 153, y = 315;
+      }
+      else if (dice_number == 34)
+      {
+          x = 206, y = 315;
+      }
+      else if (dice_number == 35)
+      {
+          x = 259, y = 315;
+      }
+      else if (dice_number == 36)
+      {
+          x = 312, y = 315;
+      }
+      else if (dice_number == 37)
+      {
+          x = 365, y = 270;
+      }
+      else if (dice_number == 38)
+      {
+          x = 365, y = 225;
+      }
+      else if (dice_number == 39)
+      {
+          x = 365, y = 180;
+      }
+      else if (dice_number == 40)
+      {
+          x = 365, y = 135;
+      }
+      else if (dice_number == 41)
+      {
+          x = 365, y = 90;
+      }
+      else if (dice_number == 42)
+      {
+          x = 365, y = 45;
+      }
+      else if (dice_number == 43)
+      {
+          x = 418, y = 45;
+      }
+      else if (dice_number == 44)
+      {
+          x = 471, y = 45;
+      }
+      else if (dice_number == 45)
+      {
+          x = 471, y = 90;
+      }
+      else if (dice_number == 46)
+      {
+          x = 471, y = 135;
+      }
+      else if (dice_number == 47)
+      {
+          x = 471, y = 180;
+      }
+      else if (dice_number == 48)
+      {
+          x = 471, y = 225;
+      }
+      else if (dice_number == 49)
+      {
+          x = 471, y = 270;
+      }
+      else if (dice_number == 50)
+      {
+          x = 524, y = 315;
+      }
+      else if (dice_number == 51)
+      {
+          x = 577, y = 315;
+      }
+      else if (dice_number == 52)
+      {
+          x = 630, y = 315;
+      }
+      else if (dice_number == 53)
+      {
+          x = 683, y = 315;
+      }
 #endif // PAW_STRUCTURE_H_INCLUDED
