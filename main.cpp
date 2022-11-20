6e3ab0b69ec8a4c6845a8d88b7052b6e555eed1a
Muhammad Asim <asimqwe2@gmail.com>
2022-11-20 10:00:00 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,3 +18,13 @@ int main()
     srand(time(NULL));
     //setting back as a back checkpoint for back button
     start_interface_animation();
+    back_button:
+    player_mod = start_interface();
+    //creating different windows on basis on return from interface
+    if (player_mod == 2)
+    {
+        ret_choice_2 = player_win2();
+        if(ret_choice_2 == 1)
+        {
+            redflag = 1;
+            start_game();
